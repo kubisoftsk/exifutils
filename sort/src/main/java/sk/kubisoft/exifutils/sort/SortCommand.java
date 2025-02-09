c00059928f46cc769c86dc3390d0d1e07f575f33
Jakub Gondar <jakubgondar@gmail.com>
2025-02-09 08:49:47 +0100

    Added write tag option to sort command

--- sort/src/main/java/sk/kubisoft/exifutils/sort/SortCommand.java
+++ sort/src/main/java/sk/kubisoft/exifutils/sort/SortCommand.java
@@ -6,8 +6,10 @@ import sk.kubisoft.exifutils.core.analysis.MediaAnalyzer;
 import sk.kubisoft.exifutils.core.file.FileExplorer;
 import sk.kubisoft.exifutils.core.file.FileMover;
 import sk.kubisoft.exifutils.core.file.MoveAction;
+import sk.kubisoft.exifutils.core.file.SetDateAction;
 import sk.kubisoft.exifutils.core.logging.Console;
 import sk.kubisoft.exifutils.core.media.MediaFile;
+import sk.kubisoft.exifutils.core.metadata.ExifDateSetter;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -23,15 +25,18 @@ public class SortCommand {
     private final MediaFileSorter mediaFileSorter;
     private final FileMover fileMover;
     private final Console console;
+    private final ExifDateSetter exifDateSetter;
 
     @Inject
     public SortCommand(FileExplorer fileExplorer, MediaAnalyzer mediaAnalyzer,
-                       MediaFileSorter mediaFileSorter, FileMover fileMover, Console console) {
+                       MediaFileSorter mediaFileSorter, FileMover fileMover, Console console,
+                       ExifDateSetter exifDateSetter) {
         this.fileExplorer = fileExplorer;
         this.mediaAnalyzer = mediaAnalyzer;
         this.mediaFileSorter = mediaFileSorter;
         this.fileMover = fileMover;
         this.console = console;
+        this.exifDateSetter = exifDateSetter;
     }
 
     public void execute(SortCommandInput input) {
@@ -51,6 +56,26 @@ public class SortCommand {
 
         console.println("Found %d media files with date, %d media files without date.", mediaFilesWithDate.size(), mediaFilesWithoutDate.size());
         mediaFilesWithoutDate.forEach((mediaFile) -> console.verboseln("No date found for %s", mediaFile.originalPath()));
+
+        // TODO duplicate refactor somehow
+        if (input.writeDate()) {
+            var setDateActions = mediaFilesWithDate.stream()
+                    .filter(exifDateSetter::needsDateTimeSet)
+                    .map(mediaFile -> new SetDateAction(mediaFile.originalPath(), mediaFile.mediaType(), mediaFile.creationDate()))
+                    .toList();
+
+            if (setDateActions.isEmpty()) {
+                console.println("No files to set date.");
+            } else {
+                console.println("Total %d files will have date set:", setDateActions.size());
+                setDateActions.forEach((action) -> console.println("%s", action));
+                if (console.confirmAction("Do you want to continue?")) {
+                    exifDateSetter.setDateTime(setDateActions);
+                } else {
+                    console.println("Aborted.");
+                }
+            }
+        }
 
         List<MoveAction> moveActions = mediaFileSorter.sort(mediaFilesWithDate, input.destinationDirectory(), input.rename());
 
