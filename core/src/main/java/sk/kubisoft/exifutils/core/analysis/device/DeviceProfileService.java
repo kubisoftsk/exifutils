22e3e4436521ce86b5cbcd5359e5ee227dc1511d
Jakub Gondar <jakubgondar@gmail.com>
2025-03-29 14:48:49 +0100

    Added KODAK M1033 profile

--- core/src/main/java/sk/kubisoft/exifutils/core/analysis/device/DeviceProfileService.java
+++ core/src/main/java/sk/kubisoft/exifutils/core/analysis/device/DeviceProfileService.java
@@ -27,6 +27,7 @@ public class DeviceProfileService {
 		files.add("nikon-d3100.yml");
 		files.add("nikon-e4800.yml");
 		files.add("olympus-fe120.yml");
+		files.add("kodak-m1033.yml");
 
 		RESOURCE_PROFILE_FILE_NAMES = Collections.unmodifiableList(files);
 	}
